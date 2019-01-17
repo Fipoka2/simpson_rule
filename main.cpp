f5e85b3bbabbfc0e898443a094e110ec82d08801
fipoka2 <smnv96@yandex.ru>
2019-01-17 15:53:14 +0300

    min max

--- main.cpp
+++ main.cpp
@@ -1,83 +1,114 @@
 #include <iostream>
 #include <cmath>
 #include <omp.h>
+#include <limits>
 
-float formula(float x);
-float integrate(float left, float right, int segments, float step);
-float altIntegrate(float left, float right, int segments, float step);
-float original(float left, float right, int segments, float step);
-float integrateBySections(float left, float right, int segments, float step);
+double formula(double x);
+double integrate(double left, double right, int segments, double step);
+double altIntegrate(double left, double right, int segments, double step);
+double original(double left, double right, int segments, double step);
+double integrateBySections(double left, double right, int segments, double step);
+
+struct m_time {
+    double min = std::numeric_limits<double>::max();
+    double average = 0;
+    double max = 0;
+};
 
 int main() {
 
-    const int SEGMENTS = 2000; // should be even number, also named as a 2n
-    const float LEFT = -3;
-    const float RIGHT = 12;
-    const float STEP = (RIGHT - LEFT) / SEGMENTS;
+    const int SEGMENTS = 10000000; // should be even number, also named as a 2n
+    const double LEFT = -3;
+    const double RIGHT = 12;
+    const double STEP = (RIGHT - LEFT) / SEGMENTS;
+    const int RUNS = 50;
 
-    float start1 = omp_get_wtime();
-    float value = original(LEFT, RIGHT, SEGMENTS, STEP);
-    float end1 = omp_get_wtime();
+    std::cout << "Function: atan(x) - 0.2*x" << std::endl;
+    std::cout << "Segments: " << SEGMENTS << std::endl;
+    std::cout << "Left: " << LEFT << std::endl;
+    std::cout << "Right: " << RIGHT << std::endl << std::endl;
 
-    std::cout << "Последовательная версия. Результат: " << value << " : time = " << end1-start1<< std::endl;
+    double start1 = omp_get_wtime();
+    double value = original(LEFT, RIGHT, SEGMENTS, STEP);
+    double end1 = omp_get_wtime();
+    std::cout << "Serial version. Value: " << value << " : time = " << end1-start1<< std::endl;
 
-    float start = omp_get_wtime();
+    double start = omp_get_wtime();
     value = integrate(LEFT, RIGHT, SEGMENTS, STEP);
-    float end = omp_get_wtime();
+    double end = omp_get_wtime();
 
-    std::cout << "Версия 1. Результат: " << value << " : time = " << end-start<< std::endl;
+    std::cout << "Version 1. Value: " << value << " : time = " << end-start<< std::endl;
 
     start = omp_get_wtime();
     value = integrateBySections(LEFT, RIGHT, SEGMENTS, STEP);
     end = omp_get_wtime();
 
-    std::cout << "Версия 2 (секции). Результат: " << value << " : time = " << end-start<< std::endl;
+    std::cout << "Version 2 (sections). Value: " << value << " : time = " << end-start<< std::endl << std::endl;
 
+    double time;
     int count = 0;
-    float res = 0;
-
-    for(int i = 1; i<50; ++i) {
+    double res = 0;
+    m_time sTime;
+    for(int i = 1; i<RUNS; ++i) {
         start = omp_get_wtime();
         value = original(LEFT, RIGHT, SEGMENTS, STEP);
         end = omp_get_wtime();
-        res += end-start;
+        time = end-start;
+        sTime.min = sTime.min < time ? sTime.min : time;
+        sTime.max = sTime.max > time ? sTime.max : time;
+        sTime.average += end-start;
         count++;
     }
-    std::cout << "Последовательная версия.Среднее время " << res/count<< std::endl;
+    sTime.average /= count;
+    std::cout << "Serial version. Min time " << sTime.min<< std::endl;
+    std::cout << "Serial version. Max time " << sTime.max<< std::endl;
+    std::cout << "Serial version. Average time " << sTime.average<< std::endl;
 
     count = 0;
     res = 0;
-    for(int i = 1; i<50; ++i) {
+    m_time forTime;
+    for(int i = 1; i<RUNS; ++i) {
         start = omp_get_wtime();
         value = integrate(LEFT, RIGHT, SEGMENTS, STEP);
         end = omp_get_wtime();
-        res += end-start;
+        time = end-start;
+        forTime.min = forTime.min < time ? forTime.min : time;
+        forTime.max = forTime.max > time ? forTime.max : time;
+        forTime.average += end-start;
         count++;
     }
-    std::cout << "Версия 1.Среднее время " << res/count<< std::endl;
-
+    forTime.average /= count;
+    std::cout << "Version 1. Min time " << forTime.min<< std::endl;
+    std::cout << "Version 1. Max time " << forTime.max<< std::endl;
+    std::cout << "Version 1. Average time " << forTime.average<< std::endl;
     count = 0;
     res = 0;
-    for(int i = 1; i<50; ++i) {
+    m_time sectionsTime;
+    for(int i = 1; i<RUNS; ++i) {
         start = omp_get_wtime();
         value = integrateBySections(LEFT, RIGHT, SEGMENTS, STEP);
         end = omp_get_wtime();
-        res += end-start;
+        time = end-start;
+        sectionsTime.min = sectionsTime.min < time ? sectionsTime.min : time;
+        sectionsTime.max = sectionsTime.max > time ? sectionsTime.max : time;
+        sectionsTime.average += end-start;
         count++;
     }
-    std::cout << "Версия 2 (секции).Среднее время " << res/count<< std::endl;
-
+    sectionsTime.average /= count;
+    std::cout << "Version 2 (sections). Min time " << sectionsTime.min<< std::endl;
+    std::cout << "Version 2 (sections). Max time " << sectionsTime.max<< std::endl;
+    std::cout << "Version 2 (sections). Average time " << sectionsTime.average<< std::endl;
 
     return 0;
 }
 
-float formula(float x) {
+double formula(double x) {
     return atan(x) - 0.2 * x;
 }
 
-float integrate(float left, float right, int segments, float step) {
-    float evenSegments = 0;
-    float oddSegments = 0;
+double integrate(double left, double right, int segments, double step) {
+    double evenSegments = 0;
+    double oddSegments = 0;
 
     #pragma omp parallel for reduction(+: evenSegments) num_threads(8)
     for (int i = 2; i < segments; i += 2 ) {
@@ -94,10 +125,10 @@ float integrate(float left, float right, int segments, float step) {
     return (step / 3) * (formula(left) + formula(right) + evenSegments + oddSegments);
 }
 
-float altIntegrate(float left, float right, int segments, float step) {
-    float evenSegments = 0;
-    float oddSegments = 0;
-    #pragma omp parallel for reduction(+: evenSegments, oddSegments) num_threads(8)
+double altIntegrate(double left, double right, int segments, double step) {
+    double evenSegments = 0;
+    double oddSegments = 0;
+    #pragma omp parallel for reduction(+: evenSegments, oddSegments) num_threads(2)
     for (int i = 1; i < segments; i++ ) {
         i%2 == 0 ? evenSegments += formula(left + step * i) : oddSegments += formula(left + step * i);
     }
@@ -107,9 +138,9 @@ float altIntegrate(float left, float right, int segments, float step) {
     return (step / 3) * (formula(left) + formula(right) + evenSegments + oddSegments);
 }
 
-float original(float left, float right, int segments, float step) {
-    float evenSegments = 0;
-    float oddSegments = 0;
+double original(double left, double right, int segments, double step) {
+    double evenSegments = 0;
+    double oddSegments = 0;
 
     for (int i = 1; i < segments; i++ ) {
         i%2 == 0 ? evenSegments += formula(left + step * i) : oddSegments += formula(left + step * i);
@@ -120,9 +151,9 @@ float original(float left, float right, int segments, float step) {
     return (step / 3) * (formula(left) + formula(right) + evenSegments + oddSegments);
 }
 
-float integrateBySections(float left, float right, int segments, float step) {
-    float evenSegments = 0;
-    float oddSegments = 0;
+double integrateBySections(double left, double right, int segments, double step) {
+    double evenSegments = 0;
+    double oddSegments = 0;
 
     #pragma omp parallel reduction(+:evenSegments, oddSegments)
     {
